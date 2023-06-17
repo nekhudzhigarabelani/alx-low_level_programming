60132c2a2ddad191718c0481846b0b57f24fd40a
nekhudzhigarabelani <nekhudzhigarabelani@gmail.com>
2023-06-17 16:41:52 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,4 +11,4 @@ printf("size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("size of float: %lu byte(s)", sizeof(float));
 return (0);
-}	
+}
