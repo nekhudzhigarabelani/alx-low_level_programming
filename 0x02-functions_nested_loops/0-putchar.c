f6318704ca47d25076e4c5235c4f824a958bf740
nekhudzhigarabelani <nekhudzhigarabelani@gmail.com>
2023-06-21 15:26:16 -0700

    README file created

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_Putchar('p');
+	_Putchar('u');
+	_Putchar('t');
+	_Putchar('c');
+	_Putchar('h');
+	_Putchar('a');
+	_Putchar('r');
+	-Putchar('\n');
+	return(0);
