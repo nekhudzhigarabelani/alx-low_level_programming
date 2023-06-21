b687c8c5a9bfb2e1f60a17e5ca59f6e003c98c50
nekhudzhigarabelani <nekhudzhigarabelani@gmail.com>
2023-06-21 12:48:10 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j <= 100; j++)
+		for (j = 0; j <= 99; j++)
 		{
 			if (i < j && i != j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
 
 				if (i != 98 || j != 99)
 				{
