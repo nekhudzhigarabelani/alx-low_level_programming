51df9501508a275821340cdc6481c8864a1b6b33
nekhudzhigarabelani <nekhudzhigarabelani@gmail.com>
2023-08-01 06:20:57 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
-#ifndef ABS_DEF
-#define ABS_DEF
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(*) ((*) > (0) ? (*) : (*) *(-1))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
+
